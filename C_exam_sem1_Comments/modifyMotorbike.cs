1c8d3bbb34e7fb4c681554b7f09dd25d06940ef4
andrey342 <61473165+andrey342@users.noreply.github.com>
2020-11-23 21:39:41 +0100

    new file modify motorbikes

---
+++ C_exam_sem1_Comments/modifyMotorbike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_exam_sem1_Comments
+{
+    public partial class modifyMotorbike : Form
+    {
+        public modifyMotorbike()
+        {
+            InitializeComponent();
+        }
+    }
+}
